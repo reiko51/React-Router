98804f0eb0c780743d879f7161ba9f20543d7c09
reiko51 <71922155+reiko51@users.noreply.github.com>
2022-08-14 15:28:05 +0900

    ルート定義の分割

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
-import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Link } from "react-router-dom";
 
-import { Home } from "./Home";
-import { Page1 } from "./Page1";
-import { Page1DetailA } from "./Page1DetailA";
-import { Page1DetailB } from "./Page1DetailB";
-import { Page2 } from "./Page2";
-import { Page2DetailC } from "./Page2DetailC";
-import { Page2DetailD } from "./Page2DetailD";
 import "./styles.css";
+import { Router } from "./router/Router";
 
 export default function App() {
   return (
@@ -19,47 +13,7 @@ export default function App() {
         <br />
         <Link to="/page2">Page2</Link>
       </div>
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route
-          path="/page1"
-          // render={(props) => (
-          render={({ match: { url } }) => (
-            <Switch>
-              {/* {console.log(url)} */}
-              <Route exact path={url}>
-                <Page1 />
-              </Route>
-              <Route path={`${url}/detailA`}>
-                <Page1DetailA />
-              </Route>
-              <Route path={`${url}/detailB`}>
-                <Page1DetailB />
-              </Route>
-            </Switch>
-          )}
-        />
-        {/* <Route path="/page2" render={() => <Page2 />} /> */}
-        {/* renderの中のreturnの中が複数になるので、()で囲む */}
-        <Route
-          path="/page2"
-          render={() => (
-            <Switch>
-              <Route exact path="/page2">
-                <Page2 />
-              </Route>
-              <Route path="/page2/detailC">
-                <Page2DetailC />
-              </Route>
-              <Route path="/page2/detailD">
-                <Page2DetailD />
-              </Route>
-            </Switch>
-          )}
-        />
-      </Switch>
+      <Router />
     </BrowserRouter>
   );
 }
---
+++ src/router/Router.jsx
@@ -0,0 +1,54 @@
+import { Switch, Route } from "react-router-dom";
+import { Home } from "../Home";
+import { Page1 } from "../Page1";
+import { Page1DetailA } from "../Page1DetailA";
+import { Page1DetailB } from "../Page1DetailB";
+import { Page2 } from "../Page2";
+import { Page2DetailC } from "../Page2DetailC";
+import { Page2DetailD } from "../Page2DetailD";
+
+export const Router = () => {
+  return (
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route
+        path="/page1"
+        // render={(props) => (
+        render={({ match: { url } }) => (
+          <Switch>
+            {/* {console.log(url)} */}
+            <Route exact path={url}>
+              <Page1 />
+            </Route>
+            <Route path={`${url}/detailA`}>
+              <Page1DetailA />
+            </Route>
+            <Route path={`${url}/detailB`}>
+              <Page1DetailB />
+            </Route>
+          </Switch>
+        )}
+      />
+      {/* <Route path="/page2" render={() => <Page2 />} /> */}
+      {/* renderの中のreturnの中が複数になるので、()で囲む */}
+      <Route
+        path="/page2"
+        render={() => (
+          <Switch>
+            <Route exact path="/page2">
+              <Page2 />
+            </Route>
+            <Route path="/page2/detailC">
+              <Page2DetailC />
+            </Route>
+            <Route path="/page2/detailD">
+              <Page2DetailD />
+            </Route>
+          </Switch>
+        )}
+      />
+    </Switch>
+  );
+};
