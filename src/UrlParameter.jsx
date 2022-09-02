f50ac0e5fc13bddc53b9955390d8fdad2062bf8e
reiko51 <71922155+reiko51@users.noreply.github.com>
2022-09-02 22:46:46 +0900

    クエリパラメータを扱う

--- src/UrlParameter.jsx
+++ src/UrlParameter.jsx
@@ -1,11 +1,16 @@
-import { useParams } from "react-router-dom";
+import { useParams, useLocation } from "react-router-dom";
 
 export const UrlParameter = () => {
   const { id } = useParams();
+  const { search } = useLocation();
+  const query = new URLSearchParams(search);
+  console.log(query);
   return (
     <div>
       <h1>UrlParameterページです</h1>
       <p>パラメータは{id}です</p>
+      <p>nameパラメータは{query.get("Name")}です</p>
+      <p>ageパラメータは{query.get("Age")}です</p>
     </div>
   );
 };
